352f203effa32097b3c36215000cffd5ae14c7e3
Julio Muller <juliodolago@gmail.com>
2021-02-01 22:51:19 -0300

    Finished form to create/edit notes

--- src/screens/Form/index.jsx
+++ src/screens/Form/index.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { TextInput, SafeAreaView, ScrollView } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { Feather } from '@expo/vector-icons'
+import { NotesContext } from '../../contexts'
 import styles from './styles'
 
 const Form = () => {
+  const noteId = useRoute().params
+  const { goBack } = useNavigation()
+  const { notes } = useContext(NotesContext)
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const handleSaveNote = () => {
+    goBack()
+  }
+
+  useEffect(() => {
+    if (noteId) {
+      const { body, title } = notes.find(({ id }) => noteId === id) // eslint-disable-line no-shadow
+
+      setTitle(title)
+      setBody(body)
+    }
+  }, [noteId])
+
   return (
-    <View style={styles.formScreen}>
-      <Text style={styles.title}>
-        Nova Nota
-      </Text>
-    </View>
+    <SafeAreaView style={styles.formScreen}>
+      <RectButton style={styles.roundedBtn} onPress={handleSaveNote}>
+        <Feather name="check" size={40} color="white" />
+      </RectButton>
+
+      <TextInput
+        style={[styles.titleInput, title ? {} : styles.placeholder]}
+        placeholder="Título"
+        onChangeText={setTitle}
+        value={title}
+        multiline
+      />
+      <ScrollView style={styles.bodyContainer}>
+        <TextInput
+          style={[styles.bodyInput, body ? {} : styles.placeholder]}
+          placeholder="Escreva sua nota"
+          onChangeText={setBody}
+          value={body}
+          multiline
+        />
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
--- src/screens/Form/styles.js
+++ src/screens/Form/styles.js
@@ -4,12 +4,42 @@ import theme from '../../theme'
 export default StyleSheet.create({
   formScreen: {
     height: '100%',
+    padding: 20,
+    backgroundColor: theme.color.main,
+  },
+  roundedBtn: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    zIndex: 999,
+    width: 70,
+    height: 70,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: theme.bgColor.main,
+    backgroundColor: theme.color.secondary,
+    borderRadius: 35,
+    elevation: 10,
   },
-  title: {
+  placeholder: {
+    fontStyle: 'italic',
+  },
+  titleInput: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
     color: 'white',
-    fontSize: 40,
+    fontSize: 32,
+    fontStyle: 'normal',
+  },
+  bodyContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 30,
+  },
+  bodyInput: {
+    lineHeight: 30,
+    color: 'white',
+    fontSize: 20,
+    fontStyle: 'normal',
   },
 })
